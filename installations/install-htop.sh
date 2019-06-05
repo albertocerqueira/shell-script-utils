467d39089837c920f5c04c23ec0625f3d7f9dbc2
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2019-06-05 15:45:25 -0300

    adicionando nova instalação.

---
+++ installations/install-htop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# Install Htop:
+# version: Any of the last
+# 
+# SO: Ubuntu
+# version: 14.04 and 16.04
+# 
+# Refer: http://deeplearning.lipingyang.org/2017/12/30/install-and-use-htop-on-ubuntu-16-04-desktop-and-server/
+
+sudo apt-get update
+sudo apt-get install htop
