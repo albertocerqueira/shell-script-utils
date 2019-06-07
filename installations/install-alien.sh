8c52c7056c080c8d14f4a5cc2a45ba27cc87b3e5
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2019-06-06 22:39:04 -0300

    Adicionando nova instalação.

---
+++ installations/install-alien.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# Install Alien:
+# version: Any of the last
+# 
+# SO: Ubuntu
+# version: 11.04, 11.10, 14.04 and 16.04
+# 
+# Refer: https://www.itzgeek.com/how-tos/linux/ubuntu-how-tos/install-rpm-packages-on-ubuntu-11-10.html
+
+sudo apt-get update
+sudo apt-get install alien
