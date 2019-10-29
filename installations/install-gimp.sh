4c9c8d2dd4ca10fd1ab167c9d62c77adec81afde
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2019-10-29 08:59:56 -0300

    adicionando novo script.

---
+++ installations/install-gimp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 
+# Install GIMP:
+# version: Any of the last
+# 
+# SO: Ubuntu
+# version: 11.04, 11.10, 14.04 and 16.04
+# 
+# Refer: https://tecadmin.net/install-gimp-on-ubuntu/
+
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt-get update
+sudo apt-get install gimp
