94ecdae772f47ed3c3b69bb8fd123e7cb5d580c0
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2020-04-01 11:47:09 -0300

    adicionando novos scripts

---
+++ installations/install-java-9.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 
+# Install Apache HTTPD:
+# version: 9
+# 
+# SO: Ubuntu
+# version: 14.04 and 16.04
+# 
+# Refer: https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04
+
+sudo apt-get update
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java9-installer
