5c20210020fda754db612ea6908e7e3529f44fab
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2020-04-01 16:42:33 -0300

    adicionando novo shell script

---
+++ installations/install-wget.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+# 
+# Install WGET:
+# version: Any of the last
+# 
+# SO: Ubuntu
+# version: 14.04 and 16.04
+# 
+# Refer: https://www.tecmint.com/install-wget-in-linux/
+
+sudo apt-get update
+sudo apt-get install wget
