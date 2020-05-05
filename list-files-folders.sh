67197eab748727815997513722f1a1ec3e770edb
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2020-05-05 15:16:33 -0300

    adicionando arquivo.

---
+++ list-files-folders.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+pasta='/home/developer/workspaces/workspaces-hub-view/git/hub-view/hub/js/*'
+
+for f in $pasta
+do
+	if [ -d "$f" ];
+	then
+	    for ff in $f/*
+	    do
+	        if [ -d "$ff" ];
+			then
+			    for fff in $ff/*
+			    do
+			        if [ -d "$fff" ];
+					then
+					    for ffff in $fff/*.js
+					    do
+					    	if [ -d "$ffff" ];
+							then
+							    for fffff in $ffff/*.js
+							    do
+							        if [ -d "$fffff" ];
+									then
+									    for ffffff in $fffff/*.js
+									    do
+									        echo "Processing $ffffff"
+									    done
+									else
+										if [ ${fffff: -3} == ".js" ]
+										then
+									    	echo "Processing $fffff"
+									    fi
+									fi
+							    done
+							else
+								if [ ${ffff: -3} == ".js" ]
+								then
+							    	echo "Processing $ffff"
+							    fi
+							fi
+					    done
+					else
+						if [ ${fff: -3} == ".js" ]
+						then
+					    	echo "Processing $fff"
+					    fi
+					fi
+			    done
+			else
+				if [ ${ff: -3} == ".js" ]
+				then
+			    	echo "Processing $ff"
+			    fi
+			fi
+	    done
+	else
+		if [ ${f: -3} == ".js" ]
+		then
+	    	echo "Processing $f"
+	    fi
+	fi
+done
