959df6ce530a2459aee5808dd7a3c86848100442
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2020-05-27 16:37:33 -0300

    adicionando novo script

--- installations/install-flameshot.sh
+++ installations/install-flameshot.sh
@@ -9,4 +9,13 @@
 # Refer: https://itsfoss.com/take-screenshot-linux/
 
 sudo apt-get update
-sudo apt install flameshot
+sudo apt install g++ build-essential qt5-default qt5-qmake qttools5-dev-tools
+sudo apt install libqt5dbus5 libqt5network5 libqt5core5a libqt5widgets5 libqt5gui5 libqt5svg5-dev
+sudo apt install git openssl ca-certificates
+git clone https://github.com/lupoDharkael/flameshot.git
+cd flameshot
+mkdir build
+cd build
+qmake ../
+sudo make
+sudo make install
