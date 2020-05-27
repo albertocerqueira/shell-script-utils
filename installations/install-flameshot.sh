877c50accc2fd1f09e981493ae20b3e635d42820
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2020-05-27 16:36:38 -0300

    adicionando novo script

---
+++ installations/install-flameshot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# Install Flameshot:
+# version: Any of the last
+# 
+# SO: Ubuntu
+# version: 14.04 and 16.04
+# 
+# Refer: https://itsfoss.com/take-screenshot-linux/
+
+sudo apt-get update
+sudo apt install flameshot
