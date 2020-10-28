39feb8903f8c97bdf332fc5e9615a068deb28acd
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2020-10-28 09:28:02 -0300

    adicionando novo arquivo.

---
+++ installations/install-intellij-idea-community.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 
+# Install Intellij:
+# version: Any of the last
+# 
+# SO: Ubuntu
+# version: Ubuntu 16.04 LTS and later
+# 
+# Refer:
+# https://www.jetbrains.com/help/idea/installation-guide.html#snap
+
+sudo apt-get update
+sudo snap install intellij-idea-community --classic --edge
