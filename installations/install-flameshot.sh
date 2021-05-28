1371ecfe6d7eea7a55f7a63294a5e18d2acfd995
Alberto Cerqueira <alberto.cerqueira1990@gmail.com>
2021-05-28 11:19:16 -0300

    atualizando arquivo

--- installations/install-flameshot.sh
+++ installations/install-flameshot.sh
@@ -9,14 +9,17 @@
 # Refer: https://itsfoss.com/take-screenshot-linux/
 # Refer: https://mithun.co/software/install-flameshot-on-ubuntu-16-04/
 
+#sudo apt-get update
+#sudo apt install g++ build-essential qt5-default qt5-qmake qttools5-dev-tools
+#sudo apt install libqt5dbus5 libqt5network5 libqt5core5a libqt5widgets5 libqt5gui5 libqt5svg5-dev
+#sudo apt install git openssl ca-certificates
+#git clone https://github.com/lupoDharkael/flameshot.git
+#cd flameshot
+#mkdir build
+#cd build
+#qmake ../
+#sudo make
+#sudo make install
+
 sudo apt-get update
-sudo apt install g++ build-essential qt5-default qt5-qmake qttools5-dev-tools
-sudo apt install libqt5dbus5 libqt5network5 libqt5core5a libqt5widgets5 libqt5gui5 libqt5svg5-dev
-sudo apt install git openssl ca-certificates
-git clone https://github.com/lupoDharkael/flameshot.git
-cd flameshot
-mkdir build
-cd build
-qmake ../
-sudo make
-sudo make install
+sudo snap install flameshot
